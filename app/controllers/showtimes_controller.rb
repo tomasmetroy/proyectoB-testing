53ee69a2a38ce944d6e0a1153bece0adf60cde39
tomasmetroy <tgperez@uc.cl>
2021-11-13 14:29:22 -0300

    showtimes main view and controller

--- app/controllers/showtimes_controller.rb
+++ app/controllers/showtimes_controller.rb
@@ -2,14 +2,6 @@
 
 class ShowtimesController < ApplicationController
   def index
-    movie = Movie.all.first
-    schedule = Schedule.all.first
-    theater = Theater.all.first
-    showtime = Showtime.new
-    showtime.movie_id = movie.id
-    showtime.schedule_id = schedule.id
-    showtime.theater_id = theater.id
-    showtime.save
     @showtimes = Showtime.all
   end
 end
